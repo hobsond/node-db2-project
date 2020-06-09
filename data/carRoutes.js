66c40b2ae127a407a5c3fad02f69a3e62e3e4b90
D'onta Hobson <hobsond26@icloud.com>
2020-06-09 17:44:04 -0400

    added the ability to read individual items

--- data/carRoutes.js
+++ data/carRoutes.js
@@ -7,6 +7,15 @@ router.get('/',(req,res)=>{
     .catch(()=>res.status(400).json({message:'could not grab cars'}))
 })
 
+router.get('/:id',(req,res)=>{
+    const id = req.params.id
+
+    db('cars')
+    .where({make:id})
+    .then(item=>res.status(200).json(item))
+    .catch(()=>res.status(400).json({message:"error can not find make"}))
+})
+
 router.post('/',(req,res)=>{
     const data = req.body
     db('cars')
